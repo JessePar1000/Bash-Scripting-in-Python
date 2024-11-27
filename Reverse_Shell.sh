c9f1eec973a190c14d4da7ccd6a46c8f01e6c0e9
JessePar1000 <Jesseparedes100@hotmail.com>
2024-11-26 16:22:43 -0800

    Update Reverse_Shell.sh

--- Reverse_Shell.sh
+++ Reverse_Shell.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+color_amarillo_1="\e[33m"
+color_amarillo_2="\e[93m"
+color_verde="\e[92m"
+color_rojo="\e[91m"
+color_reset="\e[0m"
+
+echo -e "${color_amarillo_1}Introduce the IP of the Target Machine: ${color_reset}"
